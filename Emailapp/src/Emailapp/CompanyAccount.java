55709ed712928d678c076436ee8b7c3a3f74dbc1
mgatta91 <82975737+mgatta91@users.noreply.github.com>
2021-04-29 21:41:41 -0400

    Delete Emailapp/src/Emailapp directory

--- Emailapp/src/Emailapp/CompanyAccount.java
+++
@@ -1,9 +0,0 @@
-package Emailapp;
-
-public class CompanyAccount {
-
-	public CompanyAccount(String address, String phoneNumber, String ssn, String emergencyContact, String emergencyContactNumber) {
-		
-		
-	}
-}
