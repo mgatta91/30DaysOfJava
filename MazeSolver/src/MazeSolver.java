364af0f4277ab9149c70266eceeb3d8a360bb696
Michael Gatta <michael.gatta91@gmail.com>
2021-05-08 01:53:48 -0400

    solve mazze from text file

--- MazeSolver/src/MazeSolver.java
+++ MazeSolver/src/MazeSolver.java
@@ -1,115 +1,170 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class MazeSolver {
 
-	// created 2d array to represent a  maze containing primitive "int"
-	static int [][] maze = { 
-			{1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0},
-			{0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0},
-			{0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1},
-			{1, 1, 1, 2, 0, 1, 0, 1, 0, 1, 0},
-			{0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
-			{0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1}
-	};
 	// 0 = wall
 	// 1 = path
 	// 2 = destination
 	
-	// constructor created a LinkedList type "Position" named "path"
-	static LinkedList<Position> path = new LinkedList<Position>(); 
-	
-	public static void main(String[] args) {
-		// constructor created a new object, "p", which is an instance of class "Position" 
-		Position p = new Position(4, 8); 
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		ArrayList<Maze> mazes = readMazes();	
+		
+		int i = 0;
+		//System.out.println(mazes.size()); // prints the total amount of elements in ArraList "mazes"
+		// while loop iterates through ArrayList "mazes" which contain elements  of class type "Maze" in "Mazes.txt"
+		while(i < mazes.size()) {
+			// if/else statement for "solveMaze" of type boolean when it returns true
+			if (solveMaze(mazes.get(i))) {
+				System.out.println("You won!");
+			// if/else statement for "solveMaze" of type boolean when it returns false
+			} else {
+				System.out.println("No path");
+			}
+			i++;
+		}
+	}
+	// this method allows program to read text file "Mazes.txt" and create and output an arrayList "mazes" and returned to the "main method"
+	private static ArrayList<Maze> readMazes() throws FileNotFoundException {
+		ArrayList<Maze> mazes = new ArrayList<Maze>();
+		
+		// calls constructor "Scanner" to create an object "in" is an instance of scanner 
+		// reads text file "Mazes.txt"
+		Scanner in = new Scanner(new File("Mazes.txt"));
+		// while loop that continues to read until until there is no more text to read in text file, "Mazes.txt"
+		while(in.hasNext()) {
+			// calls constructor of class "Maze" to create object "m"
+			Maze m = new Maze();
+			// parseInt - parses the string argument as a signed decimal integer.
+			int rows = Integer.parseInt(in.nextLine());
+			//System.out.println("rows equals: " + " " + rows);
+			
+			// setting "rows as the size of y variable of the 2d array, "maze"
+			// maze is a property of class "Maze"
+			m.maze = new int [rows] [];
+			// for loop iterates through each row using variable "rows" as the limit
+			for(int i = 0; i < rows; i++) {
+				// calls constructor of type "String" to read the next line of text file, "Mazes.txt"
+				// prints out every row of the mazes within the text file, "Mazes.txt"
+				String line = in.nextLine();
+				//System.out.println(line);
+				
+				// (line.split(", ")) - takes the String "line" and breaks it up into different elements separated by ", " 
+				// Arrays.stream - Takes the elements created by (line.split(", ")) and passes them into m.maze[i]
+				// .mapToInt(Integer::parseInt).toArray() - takes the stream of elements created by Arrays.stream(line.split(", ")) and converts them to a stream of type "Int"
+				m.maze[i] = Arrays.stream(line.split(", ")).mapToInt(Integer::parseInt).toArray();	
+				//System.out.println(m.maze[i][0]);
+			}
+			
+			m.start = new Position(Integer.parseInt(in.nextLine()), Integer.parseInt(in.nextLine()));
+			
+			in.nextLine(); //toss the extra space
+			
+			mazes.add(m);
+		}
+		in.close();
+		
+		return mazes;
+		
+	}
+
+	private static boolean solveMaze(Maze m) {
+		
+		Position p = m.start;
 		// "push" inserts "p" at the front of the LinkedList making it the starting point of the maze
-		path.push(p);
+		m.path.push(p);
 		
 		// repeating while statement evaluates as a boolean
 		while(true) { 
 			// tells program which row of the array
-			int y = path.peek().y; 
-			System.out.println(y);
+			int y = m.path.peek().y; 
+			//System.out.println(y);
 			// tells program which column of the array
-			int x = path.peek().x; 
-			System.out.println(x);
+			int x = m.path.peek().x; 
+			//System.out.println(x);
 			
 			// changes the value for each visited iteration within maze from 1 to 0
-			maze[y][x] = 0; 
+			m.maze[y][x] = 0; 
 			
 			// down
 			// if statements check logic and returns boolean (true or false), if this if statement returns "true"
 			// if the if statement returns "true", program will execute the if statement within it
-			if(isValid(y+1, x)) { 
+			if(isValid(y+1, x, m)) { 
 				// if the program checks the next portion of the array and finds a "2"
-				if(maze[y+1][x] == 2) {
-					System.out.println("Moved down. You won!");
+				if(m.maze[y+1][x] == 2) {
+					System.out.println("Moved down");
 					// return - stops the program from executing
-					return;
+					return true;
 				// if the program checks the next portion of the array and finds a "1"
-				}else if (maze[y+1][x] == 1) {
+				}else if (m.maze[y+1][x] == 1) {
 					System.out.println("Moved down");
 					// "push" inserts new point at the front of the LinkedList making it the point where the code will execute from
-					path.push(new Position(y+1, x));
+					m.path.push(new Position(y+1, x));
 					// continue - moves on to next line
 					continue;
 				}
 			}
 		
 			//left
-			if(isValid(y, x-1)) {
-				if(maze[y][x-1] == 2) {
-					System.out.println("Moved left. You won!");
-					return;
-				}else if (maze[y][x-1] == 1) {
+			if(isValid(y, x-1, m)) {
+				if(m.maze[y][x-1] == 2) {
 					System.out.println("Moved left");
-					path.push(new Position(y, x-1));
+					return true;
+				}else if (m.maze[y][x-1] == 1) {
+					System.out.println("Moved left");
+					m.path.push(new Position(y, x-1));
 					continue;
 				}
 			}
 			
 			//up
-			if(isValid(y-1, x)) {
-				if(maze[y-1][x] == 2) {
-					System.out.println("Moved up. You won!");
-					return;
-				}else if (maze[y-1][x] == 1) {
+			if(isValid(y-1, x, m)) {
+				if(m.maze[y-1][x] == 2) {
 					System.out.println("Moved up");
-					path.push(new Position(y-1, x));
+					return true;
+				}else if (m.maze[y-1][x] == 1) {
+					System.out.println("Moved up");
+					m.path.push(new Position(y-1, x));
 					continue;
 				}
 			}
 			
 			//right
-			if(isValid(y, x+1)) {
-				if(maze[y][x+1] == 2) {
-					System.out.println("Moved right. You won!");
-					return;
-				}else if (maze[y][x+1] == 1) {
+			if(isValid(y, x+1, m)) {
+				if(m.maze[y][x+1] == 2) {
 					System.out.println("Moved right");
-					path.push(new Position(y, x+1));
+					return true;
+				}else if (m.maze[y][x+1] == 1) {
+					System.out.println("Moved right");
+					m.path.push(new Position(y, x+1));
 					continue;
 				}
 			}
 			
 			// pop - removes the first point at the beginning of the LinkedList
-			/*Position p1 =*/ path.pop();
+			/*Position p1 =*/ m.path.pop();
 			//System.out.println("p1 = " + p1.x + " " + p1.y);
 			System.out.println("Moved back");
 			// if all values within LinkedList path are removed by "path.pop()", the maze cannot be solved
-			if(path.size() <= 0) {
-				System.out.println("No path");
-				return;
+			if(m.path.size() <= 0) {
+				return false;
 			}
 		}
+		
 	}
-	
+
 	// method used to stop program from iterating outside boundaries of array
-	public static boolean isValid(int y, int x) { 
+	public static boolean isValid(int y, int x, Maze m) { 
 		// if statement determines if "int y" or "int x" return values inside or outside boundaries of 2d array "maze"
 		if(y < 0 || 
-			y >= maze.length || 
+			y >= m.maze.length || 
 			x < 0 || 
-			x >= maze[y].length
+			x >= m.maze[y].length
 		) {
 			// returns false if "int y" or "int x" are outside of array
 			return false;
--- MazeSolver/src/Position.java
+++ MazeSolver/src/Position.java
@@ -8,4 +8,3 @@ public class Position {
 		this.y = y;
 	}
 }
-
