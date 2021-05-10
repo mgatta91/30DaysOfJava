d6def29ea042dd1852bfbd0b0d62a1f984f3406d
Michael Gatta <michael.gatta91@gmail.com>
2021-05-09 21:55:36 -0400

    hash 2

--- Collections/src/Collections.java
+++ Collections/src/Collections.java
@@ -1,21 +1,31 @@
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Collections {
 
 	public static void main(String[] args) {
-	
+		HashSet<Person> people = new HashSet<Person>();
+			
 		Person p = new Person("email@email.com", "Curry");
 		Position pos1 = new Position(10,10);
 		p.position = pos1;
 		
 		Person q = new Person("email@email.com", "Curry");
 		Position pos2 = new Position(10,10);
-		q.position = pos1;
+		q.position = pos2;
 		
-		System.out.println(p.equals(q));
-		System.out.println(p.hashCode());
-		System.out.println(q.hashCode());
-
+		people.add(p);
+		people.add(q);
+		
+		Person newPerson = new Person("email@email.com", "Curry");
+		Position newPos = new Position(10,10);
+		newPerson.position = newPos;
+		
+		System.out.println(people.contains(p));
+		
+		HashMap<String, Person> peeps = new HashMap<String, Person>();
+		peeps.put("email@email.com", newPerson);
+		System.out.println("Hashmap: " + peeps.containsKey("email@email.com"));
 	}
 
 }
--- Collections/src/Position.java
+++ Collections/src/Position.java
@@ -6,7 +6,7 @@ public class Position {
 		this.x = x;
 		this.y = y;
 	}
-
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
